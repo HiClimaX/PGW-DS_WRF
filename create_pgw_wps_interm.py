8a4585441ae469b46792ef064fa6fd9ca45de15e
Do Ngoc Khanh <14163153+ngockhanhdo1997@users.noreply.github.com>
2025-01-27 15:36:36 +0900

    Allow caching delta file to save time

--- create_pgw_wps_interm.py
+++ create_pgw_wps_interm.py
@@ -1,6 +1,7 @@
 """Create PGW WRF intermediate files"""
 
 # %%
+import logging
 from pathlib import Path
 
 import numpy as np
@@ -8,6 +9,9 @@ import pywinter.winter as pyw
 import xarray as xr
 import xesmf as xe
 from tqdm import tqdm
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
 
 # %%
 # A map from WPS intermediate variable name to CMIP6 variable name
@@ -40,7 +44,17 @@ def target_dataset(wps_inter_filepath):
     )
     ds_target = xr.Dataset(coords={"lat": lat, "lon": lon})
 
-    return ds_target, plev
+    metadata = {
+        "nlat": len(lat),
+        "nlon": len(lon),
+        "startlat": tt_geoproj["STARTLAT"],
+        "startlon": tt_geoproj["STARTLON"],
+        "deltalat": tt_geoproj["DELTALAT"],
+        "deltalon": tt_geoproj["DELTALON"],
+        "plev": plev,
+    }
+
+    return ds_target, plev, metadata
 
 
 def calculate_deltas(
@@ -52,7 +66,7 @@ def calculate_deltas(
     future,
     download_dir,
     variables=["ta", "ua", "va", "hur", "zg", "ts"],
-):
+) -> xr.Dataset:
     present = present.replace("-", "_")
     future = future.replace("-", "_")
 
@@ -88,11 +102,11 @@ def calculate_deltas(
         mean_diff = diffs[0].copy(data=np.nanmean(diffs, axis=0))
         mean_diff = mean_diff.fillna(0)
 
-        deltas[variable] = mean_diff
+        deltas[variable] = mean_diff.astype(np.float32)
 
     pbar.close()
 
-    return deltas
+    return xr.Dataset(deltas)
 
 
 def apply_pgw_delta(wps_inter_filepath, deltas, dst_dir, var_map):
@@ -146,9 +160,41 @@ def apply_pgw_delta(wps_inter_filepath, deltas, dst_dir, var_map):
 
 
 # %%
+def verify_metadata(metadata, cache_attributes):
+    for key in metadata:
+        if key not in cache_attributes:
+            return False
+
+        value = metadata[key]
+        other = cache_attributes[key]
+
+        # Check if is list,
+        if isinstance(value, (list, np.ndarray)):
+            if np.any(value != other):
+                return False
+        # Check if is float
+        elif isinstance(value, (np.floating, float)):
+            if not np.isclose(value, other):
+                return False
+        # Compare directly
+        elif value != other:
+            return False
+
+    return True
+
+
+# %%
 src_dir = Path("/data7/khanhdn/wrf/WPS/runs/era5_test/")
 dst_dir = "/data7/khanhdn/wrf/WPS/runs/era5_test_pgw/"
 wps_inter_prefix = "ERA5"
+# Calculating deltas sometimes takes a long time so we can cache the result to a
+# file if necessary, set it to None to disable caching
+cache_file = "era5_cache.nc"
+
+# src_dir = Path("/data7/khanhdn/wrf/WPS/met_input/FNL/ungrib")
+# dst_dir = "/data7/khanhdn/wrf/WPS/runs/fnl_test_pgw/"
+# wps_inter_prefix = "FNL"
+# cache_file = "fnl_cache.nc"
 
 wps_inter_filepaths = list(src_dir.glob(f"{wps_inter_prefix}:*"))
 
@@ -165,18 +211,54 @@ source_ids = [
 experiment = "ssp585"
 download_dir = Path("Download_CMIP6/Download")
 
-ds_target, plev = target_dataset(wps_inter_filepaths[0])
+ds_target, plev, metadata = target_dataset(wps_inter_filepaths[0])
+
+metadata.update(
+    {
+        "source_ids": sorted(source_ids),
+        "experiment": experiment,
+        "present": present,
+        "future": future,
+    }
+)
 
 # %%
-deltas = calculate_deltas(
-    ds_target,
-    plev,
-    source_ids,
-    experiment,
-    present,
-    future,
-    download_dir,
-)
+if cache_file is not None and Path(cache_file).exists():
+    _logger.info(f"Reading cache file {cache_file}")
+    deltas = xr.open_dataset(cache_file)
+    if not verify_metadata(metadata, deltas.attrs):
+        message = (
+            "Cache metadata mismatch. "
+            "Maybe the cache file was created with "
+            "different parameters (e.g., GCMs, experiment, years)."
+        )
+        _logger.critical(message + f"\nExpected: {metadata}\nActual: {deltas.attrs}")
+        raise RuntimeError(message)
+    _logger.info("Cache file read successfully")
+else:
+    _logger.info("Calculating deltas")
+    deltas = calculate_deltas(
+        ds_target,
+        plev,
+        source_ids,
+        experiment,
+        present,
+        future,
+        download_dir,
+    )
+
+# %%
+# Write cache if the cache file does not exist
+if cache_file is not None and not Path(cache_file).exists():
+    _logger.info(f"Writing cache file {cache_file}")
+    deltas.attrs.update(metadata)
+    deltas.to_netcdf(
+        cache_file,
+        encoding={
+            varname: {"zlib": True, "complevel": 1} for varname in deltas.variables
+        },
+    )
+
 
 # %%
 for wps_inter_filepath in wps_inter_filepaths:
